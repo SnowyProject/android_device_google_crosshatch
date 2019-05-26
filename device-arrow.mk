deb0956dd64e9e298e74b1e4389c503b2a5c5f4d
razorloves <razorloves@gmail.com>
2019-05-26 04:14:33 -0500

    b1c1: Add missing packages and props for vendor image
    Change-Id: I88bdf8b2522b751c9490820b0ef55ee53dd35d1d

--- device-arrow.mk
+++ device-arrow.mk
@@ -13,7 +13,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# Display
+PRODUCT_PACKAGES += \
+    libdisplayconfig
+
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true \
+    media.mediadrmservice.enable=true
 
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
+# Utilities
+PRODUCT_PACKAGES += \
+    libjson \
+    libtinyxml
+
+# WiFi
+PRODUCT_PACKAGES += \
+    libwifi-hal-qcom
