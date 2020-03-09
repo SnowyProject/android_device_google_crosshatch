f93ebb769faefc595e3a610fcb73c662fdc60305
P.Adarsh Reddy <padarshr@codeaurora.org>
2020-03-09 12:16:37 +0530

    b1c1: Add build broken flags in BoardConfig.mk
    BUILD_BROKEN_USES_BUILD_COPY_HEADERS
    
    Change-Id: I7b115f5bb7990c438c595b4b1ff536598786d529

--- BoardConfigArrow.mk
+++ BoardConfigArrow.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Build
+BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.lz4
 TARGET_COMPILE_WITH_MSM_KERNEL := true
@@ -14,7 +17,6 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := b1c1_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/bluecross
-
 
 # Reserve space for gapps install
 ifneq ($(ARROW_GAPPS),true)
