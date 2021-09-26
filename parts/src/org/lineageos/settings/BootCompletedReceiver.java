f6f65b24d46a4593469f6410a60a527c6fcb5c56
nullxception <enuma.alrizky@gmail.com>
2021-09-26 15:06:49 +0000

    b1c1: parts: Implement dc dimming
    Squash of the following commits:
    - parts: Move dc dimming to Settings > Display
    - parts: Implement dc dimming preference
    - parts: Disable dc dimming if it's not supported
    - parts: Add DC-Dimming QS tile
    - sepolicy: Address flicker_free denial
    
    Co-authored-by: Arian <arian.kulmer@web.de>
    Co-authored-by: itsjoeoui <joey@itsjoeoui.com>
    Co-authored-by: ReallySnow <reallysnow233@gmail.com>
    Change-Id: Ifb78b16fbfa35aae464c76e26f017cde3ec52bb4

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,19 +20,26 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.SystemProperties;
 import android.util.Log;
 import androidx.preference.PreferenceManager;
 
 import org.lineageos.settings.display.KcalUtils;
+import org.lineageos.settings.utils.FileUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final String TAG = "GoogleParts";
+    private static final String DC_DIMMING_ENABLE_KEY = "dc_dimming_enable";
+    private static final String DC_DIMMING_NODE = "/sys/module/msm_drm/parameters/flickerfree_enabled";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
         Log.d(TAG, "Received boot completed intent");
         EuiccDisabler.enableOrDisableEuicc(context);
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
+        FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0");
 
         if (KcalUtils.isKcalSupported())
             KcalUtils.writeCurrentSettings(sharedPrefs);
